d66118b78e0cd6ba97da655ba8cbf06cbb9cd364
riya4deshmukh <riyadeshmukh126@gmail.com>
2025-08-28 23:55:06 +0530

    rmv logout

--- educorebackend/index.js
+++ educorebackend/index.js
@@ -11,29 +11,12 @@ const student = require("./controller/studentcontoller.js")
 const app = express();
 app.use(cors());
 
-
-  
-
 app.use(express.json());
 app.use("/api", user);
 app.use("/api", college);
 app.use("/api", mark);
 app.use("/api", role);
-app.use("/api", student);
-
-// Logout middleware
-const Logout = (req, res, next) => {
-    console.log(`${req.originalUrl} - ${new Date().toLocaleString()}`, "Logout middleware triggered");
-    next();
-};
-
-app.use("/api", Logout, user);
-app.use("/api", Logout, college);
-app.use("/api", Logout, mark);
-app.use("/api", Logout, role);
-app.use("/api", Logout, student);
- 
- 
+app.use("/api", student); 
 
 app.listen(7000 , ()=>{
     console.log("http://localhost:7000/user")
